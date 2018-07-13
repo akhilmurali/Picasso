4cd3090cb689b3bbfc607dbf551f5e7ef3ff25e8
Akhil Murali <github@akhilmurali.com>
2018-07-13 13:05:31 +0530

    Add support for promise in mongoose

--- ms08-imgur/index.js
+++ ms08-imgur/index.js
@@ -1,15 +1,26 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// serve static files from /public
-app.use(express.static(__dirname + '/template'));
+require('dotenv').config();
+let mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://${process.env.uname}:${process.env.pass}@ds135421.mlab.com:35421/imgur_macrocozm`,
+  { useNewUrlParser: true })
+  .then(() => { console.log('mongodb connected') }, () => { console.log('Connection error to mongo db'); })
+  .catch((err) => { console.log(err); });
+
+// serve static files from /public:
+app.use(express.static(__dirname + '/public'));
 
 // include routes
 var routes = require('./routes/router');
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('File Not Found');
   err.status = 404;
   next(err);
@@ -17,7 +28,7 @@ app.use(function(req, res, next) {
 
 // error handler
 // define as the last app.use callback
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json('error', {
     message: err.message,
